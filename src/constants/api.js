a2793c88b86081d2ffcee793b390e782ebea423a
whitecity01 <kh081104@gmail.com>
2024-10-07 19:38:27 +0900

    feat: api 관리 파일 추가

---
+++ src/constants/api.js
@@ -0,0 +1,3 @@
+export const SERVER_IP = process.env.REACT_APP_SERVER_API;
+export const KAKAO_REST_API = process.env.REACT_APP_KAKAO_REST_API;
+export const KAKAO_MAP_API = process.env.REACT_APP_KAKAO_MAP_API;
