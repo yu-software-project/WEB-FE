34d1007e1ddafb07ce4e598cf3376e1b61a1e794
whitecity01 <kh081104@gmail.com>
2024-10-07 19:39:15 +0900

    feat: 루트 레이아웃 구현

---
+++ src/pages/RootLayout.jsx
@@ -0,0 +1,28 @@
+import { Outlet } from "react-router-dom";
+import { reissueToken } from "services/auth/token";
+import { getAuthToken, getRefreshToken } from "utils/token";
+
+const RootLayout = () => {
+  return <Outlet />;
+};
+
+export default RootLayout;
+
+export const loader = async ({ request }) => {
+  const { pathname } = new URL(request.url);
+  const refreshToken = getRefreshToken();
+  const { accessToken, setAccessToken } = getAuthToken();
+
+  if (accessToken && pathname === "/") {
+    //return redirect("/home");
+  }
+
+  // accessToken 미존재, refreshtoken 존재
+  if (!accessToken && refreshToken) {
+    const response = await reissueToken(refreshToken);
+    const { accessToken: newAccessToken } = response.data;
+    setAccessToken(newAccessToken);
+    //return redirect(pathname);
+  }
+  return null;
+};
