8d4903d28788ddcd88ab0ec986fdf8ae5d63e379
whitecity01 <kh081104@gmail.com>
2024-11-06 01:13:23 +0900

    feat: 봉사 목차 유틸 컴포넌트 추가

---
+++ src/components/home/volunteer/upload/DetailName.jsx
@@ -0,0 +1,26 @@
+const DetailName = ({ path }) => {
+  let content1, content2, content3;
+  switch (path) {
+    case "upload":
+      content1 = "봉사 제목";
+      content2 = "일정";
+      content3 = "모집인원";
+      break;
+    case "detail":
+      content1 = "상세정보";
+      content2 = "";
+      content3 = "";
+      break;
+    default:
+      content1 = "";
+  }
+  return (
+    <div className="volunteer-upload-detail-name-container">
+      <div className="content1">{content1}</div>
+      <div className="content2">{content2}</div>
+      <div className="content3">{content3}</div>
+    </div>
+  );
+};
+
+export default DetailName;
